1a3780cabe2b3fccbf98f98874ca89e818da5f28
free6nk33 <free6nk33@gmail.com>
2025-01-10 15:27:26 +0100

    Update RFID-LED-SERVOMOTOR.py

--- RFID-LED-SERVOMOTOR.py
+++ RFID-LED-SERVOMOTOR.py
@@ -1,8 +1,19 @@
-import machine
-import time
+'''    
+    GROUPE 3
+
+    Thibaud
+    Cameron
+    Antoine
+    Célian
+
+'''
+
 from mfrc522 import MFRC522
 from machine import Pin, PWM
+import time
 
+
+# CONNEXION MODULE RFID
 rdr = MFRC522(sck=18, mosi=23, miso=19, rst=22, cs=21)
 # - D22  (RST) 
 # - D23 (MOSI)
@@ -10,65 +21,87 @@ rdr = MFRC522(sck=18, mosi=23, miso=19, rst=22, cs=21)
 # - D18 (SCK)
 # - D21 (SDA)
 
-
+# CONNEXION MODULE LED
 pin_led = Pin(17, Pin.OUT)
-
 # - D17 (LED)
 
-
-
-# SERVO MOTEUR
-#-----------------------------------
+# CONNEXION MODULE SERVO
 servo_pin = 13
+# - D13 (SERVO)
+
+# initialisation du servo
 pwm = PWM(Pin(servo_pin))
 pwm.freq(50)
-
 min = 26   # (0°)
 max = 123  # (180°)
 
+#--------------------------------------------
+#               FUNCTIONS                   |
+#--------------------------------------------
 def move_servo(angle):
     duty = min + (max - min) * (angle / 180)
     pwm.duty_u16(int(duty * 65535 / 1000))
-#-----------------------------------
+
+def allumer_led():
+    pin_led.value(1)
+    
+def eteindre_led():
+    pin_led.value(0)
+#--------------------------------------------
 
 
-print("\n Presenter le badge")
 
-while True:
-    (stat, tag_type) = rdr.request(rdr.REQIDL)
-    if stat == rdr.OK:
-        (stat, raw_uid) = rdr.anticoll()
+def main():
+    '''
+    Ce script gère la lecture et la gestion des badges RFID via un lecteur de badge, si le badge est valide alors des actions sont effectuées dans le cas contraire rien ne se passe.
+    Dans ce script on utilise 3 modules sur les 4 que nous avions pour les interconnecter.
+
+    Entrées :
+        - badge RFID présenté au lecteur
+        
+    Sorties :
+        YES -> action (allumer une LED et déplacer un servo-moteur)
+        NO -> aucune action (rien ne se passe aucune LED allumée ou servo déplacé)
+    '''
+
+    print("\n Presenter le badge")
+
+    while True:
+
+        # attente de la lecture du badge
+        (stat) = rdr.request(rdr.REQIDL)
+        
+        # lecture du badge
         if stat == rdr.OK:
-            #print("\nBadge détecté !")
-            #print(" - Type : %d" % tag_type)
-            #print(" - UID : %02x.%02x.%02x.%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+            (stat, raw_uid) = rdr.anticoll()
+            if stat == rdr.OK:
 
-            if rdr.select_tag(raw_uid) == rdr.OK:
-                key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-                if rdr.auth(rdr.AUTHENT1A, 8, key, raw_uid) == rdr.OK:
-                    data = rdr.read(8)
-                    #print(" - Données : %s" % data)
+                if rdr.select_tag(raw_uid) == rdr.OK:
+                    key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+                    if rdr.auth(rdr.AUTHENT1A, 8, key, raw_uid) == rdr.OK:
+                        data = rdr.read(8)
 
-                    if data == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]:
+                        # si le badge est valide alors on effectue les actions
+                        if data == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]:
+                            
+                            allumer_led() 
+                            move_servo(0)
+                            
+                            time.sleep(2)
+                            eteindre_led()
+                            move_servo(180)
+                            
+                        else:
+                            pin_led.value(0) 
+                            #servo.value(0)
                         
-                        pin_led.value(1)  
-                        move_servo(0)
-                        time.sleep(2)
-                        pin_led.value(0)
-                        move_servo(180)
-                        
+                        rdr.stop_crypto1()
                     else:
-                        pin_led.value(0) 
-                        #servo.value(0)
-                    
-                    rdr.stop_crypto1()
+                        print("Erreur de lecture")
                 else:
-                    print("Erreur de lecture")
-            else:
-                print("Erreur de sélection du badge")
-    time.sleep(1)
+                    print("Erreur de sélection du badge")
+        time.sleep(1)
 
 
-
-
-
+if __name__ == '__main__':
+    main()
